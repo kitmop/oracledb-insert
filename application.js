74012420e819f761ea8a039cc0e48d4b66009086
kitmop <mustafatuglu@yandex.com>
2022-08-05 14:17:44 +0300

    Update application.js

--- application.js
+++ application.js
@@ -1,11 +1,11 @@
 const oracledb = require('oracledb');
-const readline = require('readline')
-const fs = require('fs')
-const csv = require('fast-csv')
+const fs = require('fs');
+const csv = require('fast-csv');
 
 //read from csv
-const file_path = "data\\australian.csv"
-rows = []
+const file_name = "australian"
+const file_path = `data\\${file_name}.csv`;
+const rows = [];
 fs.createReadStream(file_path)
     .pipe(csv.parse({ headers: true }))
     .on('error', error => console.error(error))
@@ -25,33 +25,33 @@ async function run() {
     });
     
     //create table
-    const table_name = 'fatih_australian'
-    var headers = Object.keys(rows[0])
-    var sql_create = `create table ${table_name}(`
+    const table_name = `fatih_${file_name}`;
+    const headers = Object.keys(rows[0]);
+    var sql_create = `create table ${table_name}(`;
 
     
     for (let i = 0; i < headers.length; i++) {
-      sql_create += `${headers[i]} varchar2(4000), `
+      sql_create += `${headers[i]} varchar2(4000), `;
     }
-    sql_create += `primary key (${headers[0]}))` //assuming that the first column is the primary key
-    console.log(sql_create)
+    sql_create += `primary key (${headers[0]}))`; //assuming that the first column is the primary key
+    console.log(sql_create);
     
-  await connection.execute(sql_create)
-  console.log("Table created successfully")
+  await connection.execute(sql_create);
+  console.log("Table created successfully");
 
 
     //insert data into the table
-    var sql_insert = `insert into ${table_name} values (`
+    var sql_insert = `insert into ${table_name} values (`;
     for (let i =0; i<headers.length-1; i++) {
-      sql_insert += `:${i}, `
+      sql_insert += `:${i}, `;
     }
-    sql_insert += `:${headers.length-1})`
+    sql_insert += `:${headers.length-1})`;
     for (let i = 0; i < rows.length; i++) {
       connection.execute(
         sql_insert, Object.values(rows[i]));
-        connection.commit()
+        connection.commit();
     }
-    console.log("Data inserted succesfully.")
+    console.log("Data inserted succesfully.");
   
   } catch (err) {
     console.log(err);
